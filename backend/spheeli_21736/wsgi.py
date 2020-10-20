f406997e7239a57e05ad313d78cea2a1af959ed4
Crowdbotics <team@crowdbotics.com>
2020-10-20 03:51:11 +0000

    Update models

--- backend/spheeli_21736/wsgi.py
+++ backend/spheeli_21736/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spheeli_21736.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spheeli_21736.settings")
 
 application = get_wsgi_application()
